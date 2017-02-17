10ed5a1b76167f9f6dd9a9afee3d50b1ce5a274a
Adam <veroci@gmail.com>
2017-02-17 12:27:09 +0100

    overtime commit!

--- Final/JumpIn.java
+++ Final/JumpIn.java
@@ -7,27 +7,27 @@ public class JumpIn {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> alist = new ArrayList<>();
-        alist.add(1);
-        alist.add(2);
-        alist.add(3);
-        alist.add(3);
-        alist.add(1);
-        alist.add(4);
-        alist.add(3);
+        ArrayList<Integer> numbersList = new ArrayList<>();
+        numbersList.add(1);
+        numbersList.add(2);
+        numbersList.add(3);
+        numbersList.add(3);
+        numbersList.add(1);
+        numbersList.add(4);
+        numbersList.add(3);
 
-        duplicateRemover(alist);
+        duplicateRemover(numbersList);
 
-        System.out.println(alist);
+        System.out.println(numbersList);
     }
 
     public static ArrayList<Integer> duplicateRemover(ArrayList<Integer> numbers){
-        for(int i=0; i<numbers.size(); i++){
+        for(int i=0; i < numbers.size(); i++){
             int x = numbers.get(i);
-            for(int j=1; j<((numbers.size())-1); j++){
+            for(int j = 1; j<(numbers.size()-1); j++){
                 int y = numbers.get(j);{
-                    if (x==y){
-                        numbers.remove(y);
+                    if (x==y && numbers.indexOf(i)!=numbers.indexOf(j)){
+                        numbers.remove(j);
                     }
                 }
             }
